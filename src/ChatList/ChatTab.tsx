3a66d9ddaebc2142c66e2b6ecda6204c9d4e3962
Maria <dobrydnyk.m@gmail.com>
2026-06-20 17:11:22 +0300

    Create navigation buttons

---
+++ src/ChatList/ChatTab.tsx
@@ -0,0 +1,21 @@
+import { tabs } from './tabs'
+
+export function ChatTab() {
+    return (
+        <div className="flex items-center justify-around px-4 py-4 border-t border-[#454545] bg-white mt-auto">
+        {tabs.map(tab => {
+                const Icon = tab.icon;
+
+                return (
+                    <button
+                        key={tab.id}
+                        className="flex items-center text-[#454545] hover:text-zinc-400 transition-colors"
+                    >
+                        <Icon size={24} />
+                    </button>
+                )
+            }
+        )}
+    </div>
+    )
+}
