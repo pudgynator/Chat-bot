b40c7639313f4d4c1f2813b2888b5588c5dbf65f
Maria <dobrydnyk.m@gmail.com>
2026-06-20 17:12:26 +0300

    Fix mistakes

--- src/ChatList/ChatTab.tsx
+++ src/ChatList/ChatTab.tsx
@@ -3,7 +3,7 @@ import { tabs } from './tabs'
 export function ChatTab() {
     return (
         <div className="flex items-center justify-around px-4 py-4 border-t border-[#454545] bg-white mt-auto">
-        {tabs.map(tab => {
+        {tabs.map(tab => { 
                 const Icon = tab.icon;
 
                 return (
