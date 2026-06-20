1103d73c44ebdb9450044e08ee45801fabaf6dca
Maria <dobrydnyk.m@gmail.com>
2026-06-20 19:09:54 +0300

    Change tabs style

--- src/ChatList/ChatTab.tsx
+++ src/ChatList/ChatTab.tsx
@@ -1,17 +1,27 @@
 import { tabs } from './tabs'
+import { useState } from "react";
 
 export function ChatTab() {
+    const [activeTab, setActiveTab ] = useState('chats');
+
     return (
-        <div className="flex items-center justify-around px-4 py-4 border-t border-[#454545] bg-white mt-auto">
-        {tabs.map(tab => { 
+        <div className="flex items-center justify-around px-4 py-3 border-t-2 border-zinc-100 bg-white mt-auto">
+        {tabs.map(tab => {
                 const Icon = tab.icon;
 
                 return (
                     <button
                         key={tab.id}
-                        className="flex items-center text-[#454545] hover:text-zinc-400 transition-colors"
+                        onClick={() => setActiveTab(tab.id)}
+                        className={`
+                            flex items-center transition-colors 
+                            ${activeTab === tab.id
+                            ? "text-[#454545]"
+                            : "text-zinc-400"
+                            }
+                        `}
                     >
-                        <Icon size={24} />
+                        <Icon size={24} fill={activeTab === tab.id ? "#454545": "#a1a1aa" }/>
                     </button>
                 )
             }
