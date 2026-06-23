b7453ca5b3afd799938516e9c4f1983808d0c77c
Maria <dobrydnyk.m@gmail.com>
2026-06-23 22:05:09 +0300

    Create data folder

---
+++ src/data/chats.tsx
@@ -0,0 +1,34 @@
+import { getHours, getMinutes } from "date-fns";
+
+export type ChatProps = {
+    id: string;
+    name: string;
+    lastMessage: string;
+    time: string;
+    avatar?: string; 
+}
+
+const hours = getHours(new Date());
+const minutes = getMinutes(new Date())
+
+export const chats: ChatProps[] = [
+    {
+        id: '1',
+        name: "Maria",
+        lastMessage: "Hi!",
+        time: "12:30",
+    },
+    {
+        id: '2',
+        name: "Alex",
+        lastMessage: "See you",
+        time: "11:20",
+    },
+    {
+        id: '3',
+        name: "Help Bot",
+        lastMessage: "How can I help you?",
+        time: `${hours}:${minutes}`,
+        avatar: `images/bot-ava.jpg`
+    },
+];
