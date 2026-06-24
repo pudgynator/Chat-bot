7671081db00181ba2ae7305227955f837b1a3810
Maria <dobrydnyk.m@gmail.com>
2026-06-24 13:06:43 +0300

    Change styles

--- src/Chat/ChatHeader.tsx
+++ src/Chat/ChatHeader.tsx
@@ -8,16 +8,15 @@ export function ChatHeader({chat }: ChatHeaderProps) {
     console.log(chat);
 
     return(
-        <div className="flex  items-center bg-white/70 backdrop-blur-md border border-white/30 shadow-sm w-full rounded-2xl ">
+        <div className="flex items-center gap-1.5 px-1 py-1 bg-white/70 backdrop-blur-md border border-white/30 shadow-sm w-full rounded-full ">
             <img
                     src={ chat.avatar ?? '/images/default-ava.jpg'} 
                     alt="User avatar" 
-                    className=" rounded-full w-8 h-8 ml-1 mt-0.5 mb-0.5"
+                    className=" rounded-full w-8 h-8 "
             />
-            <button className="flex flex-col gap-0.5 rounded-full px-2 py-1">
-
-                <span className="font-sm">{chat.name}</span>
-                <span className="text-xs">Online</span>
+            <button className="flex flex-col items-start gap-0.5">
+                <span className="text-sm leading-none">{chat.name}</span>
+                <span className="text-xs leading-none text-zinc-500">online</span>
             </button>
         </div>
     )
