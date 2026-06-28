cf49b32788dabf26e1453f44b7e3404f7b514cd4
Maria <dobrydnyk.m@gmail.com>
2026-06-28 16:15:55 +0300

    Add exit button in Chat Header

--- src/Chat/ChatHeader.tsx
+++ src/Chat/ChatHeader.tsx
@@ -1,4 +1,5 @@
 import type { ChatProps } from "../data/chats";
+import ArrowPrev  from "../assets/ArrowPrev";
 
 export type ChatHeaderProps = {
     chat: ChatProps;
@@ -8,16 +9,23 @@ export function ChatHeader({ chat }: ChatHeaderProps) {
     console.log(chat);
 
     return(
-        <div className="flex items-center gap-2 px-1 py-1 bg-white/70 backdrop-blur-md border border-white/30 shadow-sm w-full rounded-full ">
-            <img
-                    src={ chat.avatar ?? '/images/default-ava.jpg'} 
-                    alt="User avatar" 
-                    className=" rounded-full w-8 h-8 "
-            />
-            <button className="flex flex-col items-start gap-1">
-                <span className="text-sm leading-none">{chat.name}</span>
-                <span className="text-xs leading-none text-zinc-500">online</span>
-            </button>
+        <div className="flex items-center gap-3">
+            <div className="w-max md:hidden bg-white/70 backdrop-blur-md border border-white/30 shadow-sm rounded-full p-1">
+                <ArrowPrev className="w-8 h-8 font-sm"/>
+            </div>
+
+            <div className="flex items-center gap-2 px-1 py-1 bg-white/70 backdrop-blur-md border border-white/30 shadow-sm w-full rounded-full ">
+                <img
+                        src={ chat.avatar ?? '/images/default-ava.jpg'} 
+                        alt="User avatar" 
+                        className=" rounded-full w-8 h-8 "
+                />
+                <button className="flex flex-col items-start gap-1">
+                    <span className="text-sm leading-none">{chat.name}</span>
+                    <span className="text-xs leading-none text-zinc-500">online</span>
+                </button>
+            </div>
+
         </div>
     )
 }
