8e76581e9d036fb1bd57e56eba9ac3e77052a06f
Maria <dobrydnyk.m@gmail.com>
2026-07-03 16:49:37 +0300

    Add and style send icon

--- src/Chat/ChatBody.tsx
+++ src/Chat/ChatBody.tsx
@@ -12,12 +12,16 @@ export function ChatBody(){
             <form 
                 action="" 
                 onSubmit={HandleSubmit}
-                className="w-full "
+                className="w-full flex items-center gap-2"
             >
                 <input 
                     type="text" 
                     className="w-full rounded-full px-3 py-2 text-zinc-900 caret-zinc-900 focus:outline-none bg-white/70 backdrop-blur-md border border-white/30 shadow-sm" 
-                    placeholder="Write a message..."/>
+                    placeholder="Write a message..."
+                />
+                <button type="submit" onClick={() => HandleSubmit} className=" flex w-10 h-10 rounded-full bg-white/70 backdrop-blur-md border border-white/30 shadow-sm flex items-center justify-center">
+                    <img src="/images/send-symbol.svg" alt="send icon"/>
+                </button>
             </form>
         </div>
     )
