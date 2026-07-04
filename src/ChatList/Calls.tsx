dcdbd100fb28d588c2aaf1d4ff5e05200db4c552
Maria <dobrydnyk.m@gmail.com>
2026-07-04 14:38:39 +0300

    Implement Calls component

---
+++ src/ChatList/Calls.tsx
@@ -0,0 +1,15 @@
+
+
+export function Calls() {
+    return (
+        <div className="flex flex-col w-full">
+            <button className="flex text-sm font-light text-[#454545] border-t border-zinc-100 px-2 py-2">
+                Create New Call
+            </button>
+            <div className="text-zinc-400 text-xs bg-zinc-100 w-full p-1">
+                Recent calls
+            </div>
+
+        </div>
+    )
+}
