ddc48e71973bcd1be9e24678cec022ed6e11ab77
Maria <dobrydnyk.m@gmail.com>
2026-07-06 17:00:41 +0300

    Create profile button in settings

--- src/ChatList/Settings.tsx
+++ src/ChatList/Settings.tsx
@@ -1,8 +1,9 @@
+import ArrowPrev from "../assets/ArrowPrev";
 
 export function Settings() {
     return(
         <div className="px-4">
-            <div className="flex gap-2 bg-zinc-100 rounded-2xl px-3 py-4">
+            <div className="relative flex gap-2 bg-zinc-100 rounded-2xl px-3 py-4">
                 <img
                     src='/images/default-ava.jpg'
                     alt="User avatar" 
@@ -11,6 +12,11 @@ export function Settings() {
                 <span className="text-sm font-medium">
                     User
                 </span>
+                <button 
+                    className="absolute p-1 rotate-180 right-0 top-8"
+                >
+                    <ArrowPrev className="w-5 h-5 font-sm " fill="#a1a1aa"/>
+                </button>
             </div>
         </div>
     )
