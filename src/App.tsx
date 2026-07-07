76cb4e645aef0439b4437718af562406db0605e4
Maria <dobrydnyk.m@gmail.com>
2026-07-07 17:35:59 +0300

    Update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Layout } from './components/Layout/Layout';
+import axios from "axios";
+import { Layout } from "./components/Layout/Layout";
+import { useEffect } from "react";
 
 function App() {
+    const fetchAPI = async () => {
+        const response = await axios.get("http://localhost:3000/api");
+        console.log(response.data);
+    };
 
-  return (
-    <Layout/>
-  )
+    useEffect(() => {
+        fetchAPI();
+    }, [])
+
+    return <Layout />;
 }
 
-export default App
+export default App;
