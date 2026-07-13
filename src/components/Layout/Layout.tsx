f0666b56361c64287c8077ed8c6218c065d833b1
Maria <dobrydnyk.m@gmail.com>
2026-07-13 13:24:07 +0300

    Update data

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from  'axios';
 import { MainContent } from "./MainContent";
 import { Sidebar } from "./Sidebar";
-import { type ChatProps, chats } from "../../data/chats";
+import { type ChatProps } from "../../data/chats";
 
 
 export function Layout() {
     const [selectedChat, setSelectedChat] = useState<ChatProps | null>(null);
+    const [chats, setChats] = useState<ChatProps[]>([]);
     const [activeTab, setActiveTab] = useState("chats");
+
+    useEffect(() => {
+        const fetchChats = async () => {
+            try {
+                const token = localStorage.getItem('token');
+
+                const response = await axios.get(
+                    'http://localhost:3000/api/chats',
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                setChats(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchChats();
+    }, [])
+
     return (
         
         <div className="flex relative p-0 md:p-2 h-screen w-screen overflow-hidden bg-[url('/images/bg-image.jpg')] bg-cover bg-center bg-no-repeat" > 
