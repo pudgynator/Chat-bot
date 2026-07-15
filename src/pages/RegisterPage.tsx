29daa2fe1515431c00cf7fcaf5430ad4cf7adc3c
Maria <dobrydnyk.m@gmail.com>
2026-07-15 17:13:19 +0300

    Update styles for login and register forms

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -28,37 +28,39 @@ export function RegisterPage() {
             bg-[url('/images/bg-image.jpg')] bg-cover bg-center bg-no-repeat" 
         > 
             <div className="flex flex-col items-center justify-center
-                bg-white/90 backdrop-blur-md border border-white/30 shadow-sm rounded-2xl 
+                bg-zinc-200 backdrop-blur-md border border-white/30 shadow-sm md:rounded-2xl 
                 md:w-[350px] md:min-w-[300px] w-full h-full bg-white
             ">
                 <h1 className="text-xl font-semibold">Sign in to Messenger</h1>
-                <h3 className="flex flex-col items-center text-zinc-400 text-sm mb-4">Please enter your phone number 
+                <h3 className="flex flex-col items-center text-zinc-400 text-sm mb-4">Please enter your phone number, 
                     <span className="">
-                        and name for registration.
+                        name and password for registration.
                     </span>
                 </h3>
-                <form className="flex flex-col gap-4 w-full p-4" onSubmit={handleSumbit}>
-                    <input 
-                        className="rounded-xl border border-zinc-400 px-2 py-2.5 text-xs opacity-80" 
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                        type="tel" 
-                        pattern="^\+380\d{9}$"
-                        placeholder="+380 ... ... ..." />
-                    <input 
-                        className="rounded-xl border border-zinc-400 px-2 py-2.5 text-xs opacity-80" 
-                        onChange={(e) => setName(e.target.value)}
-                        value={name}
-                        type="text" 
-                        placeholder="Name" />
-                    <input
-                        className="rounded-xl border border-zinc-400 px-2 py-2.5 text-xs opacity-80 mb-2"
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password"
-                    />
-                    <button type='submit' className="bg-zinc-600 text-white rounded-xl p-2 font-medium cursor-pointer">SIGN IN</button>
+                <form className="flex flex-col gap-4 md:w-full w-[350px] p-4" onSubmit={handleSumbit}>
+                    <div className="rounded-3xl px-4 bg-white shadow-sm">
+                        <input 
+                            className="w-full  text-sm border-b border-zinc-300 bg-transparent py-3 outline-none" 
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            type="tel" 
+                            pattern="^\+380\d{9}$"
+                            placeholder="+38 0.. ... ...." />
+                        <input 
+                            className="w-full text-sm border-b border-zinc-300 bg-transparent py-3 outline-none" 
+                            onChange={(e) => setName(e.target.value)}
+                            value={name}
+                            type="text" 
+                            placeholder="Name" />
+                        <input
+                            className="w-full text-sm py-2 outline-none mb-2"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password"
+                        />
+                    </div>
+                    <button type='submit' className="bg-zinc-600 text-white rounded-3xl p-2 font-medium cursor-pointer">SIGN IN</button>
                 </form>
                 <span className="text-zinc-400 text-sm mb-4">
                     Already have an account? {" "}
