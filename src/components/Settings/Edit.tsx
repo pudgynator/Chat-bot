82ebe15f0f07d01f8dcee0a1fc2b76c70eb5051a
Maria <dobrydnyk.m@gmail.com>
2026-07-18 14:50:28 +0300

    Update structure and styles

--- src/components/Settings/Edit.tsx
+++ src/components/Settings/Edit.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import ArrowPrev from "../../assets/ArrowPrev";
+import { useState } from "react";
 
 type EditProps = {
     onClose: () => void;
@@ -9,6 +11,9 @@ export function Edit({ onClose }: EditProps) {
     const user = storedUser ? JSON.parse(storedUser) : null;
     const navigate = useNavigate();
 
+    const [name, setName] = useState(user?.name || '');
+    const [phone, setPhone] = useState(user?.phone || '');
+
     const handleLogout = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('user');
@@ -17,37 +22,38 @@ export function Edit({ onClose }: EditProps) {
     }
     return (
         <div className="flex flex-col h-full px-4">
-            <div className="flex items-center justify-between py-2">
+            <div className="flex absolute top-0 left-0 right-0 min-w-[300px] items-center justify-between py-2 px-4 z-10">
                 <button 
                     onClick={onClose}
-                    className="p-2 rounded-full bg-zinc-100 shadow-sm"
+                    className="p-2 rounded-full bg-zinc-100 shadow-lg"
                 >
-                    <img src="" alt="exit arrow" />
+                    <ArrowPrev className="w-5 h-5 font-sm " fill="#black"/>
                 </button>
-                <h2>
-                    Edit Profile
-                </h2>
-                <button className="p-2 rounded-full bg-zinc-100 shadow-sm">
+                <button className="p-2 rounded-full text-zinc-900 bg-zinc-100 shadow-lg">
                     Done
                 </button>
             </div>
-            <div className="relative flex gap-2 bg-zinc-100 rounded-2xl px-3 py-4">
+            <div className="relative flex gap-2 bg-zinc-100 rounded-2xl px-3 py-4 mt-4 shadow-sm mb-2">
                 <img
                     src='/images/default-ava.jpg'
                     alt="User avatar" 
                     className=" rounded-full w-15 h-15"
                 />
-                <div className="flex flex-col gap-0.5">
+                <div className="flex flex-col gap-1 w-full px-2">
                     <input 
-                        className="text-sm font-medium outline-none border-b border-white"
-                        defaultValue={user?.name}
+                        className="text-sm w-full font-weigth-300 outline-none border-b border-zinc-300 py-1"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                     />
 
-                    <input className="text-sm font-medium outline-none"
-                        defaultValue={user?.phone}
+                    <input 
+                        className="text-xs font-weigth-300 outline-none py-1"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
                     />
                 </div>
             </div>
+            <p className="text-xs text-zinc-400 px-3">Enter your name, phone number and add a profile photo.</p>
             <button onClick={handleLogout} className="mt-auto mb-2 bg-zinc-100 shadow-sm rounded-3xl px-3 py-3">
                 <span className="text-red-500 font-light">Log Out</span>
             </button>
