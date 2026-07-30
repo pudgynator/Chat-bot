038678d6b370dc1855cb34dc7eaadb1145dbc1fe
Maria <dobrydnyk.m@gmail.com>
2026-07-30 14:36:10 +0300

    Update chat props

--- src/types/Chats.ts
+++ src/types/Chats.ts
@@ -2,11 +2,12 @@ export type ChatProps = {
     id: string;
     _id?: string;
     name: string;
+    updatedAt: string;
     lastMessage?: string;
     lastMessageSender?: string;
     lastSeen?: string;
     time?: string;
     avatar?: string; 
-    updatedAt: string;
+    unreadCount?: number;
     members?: Array<string | { _id: string; id?: string; name?: string; avatar?: string }>;
 }
